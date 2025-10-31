5574c949f196a61b26e2da395d3d4abae86921cd
kyle-parker-1500 <kylerparker1500@gmail.com>
2025-10-31 10:32:24 -0700

    NOT WORKING... yet -> working on home page

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from random_images import random_three as rt
 
 random_dict = rt()
-lists = [v for k, v in random_dict.items()]
-print(lists)
+# lists = [v for k, v in random_dict.items()]
+# print(lists)
 
-for i in random_dict:
-    print(random_dict[i])
+# for i in random_dict:
+#     print(random_dict[i])
+
+im1_name = random_dict[0][1]
+im1_title = random_dict[0][0]
+
+im2_name = random_dict[1][1]
+im2_title = random_dict[1][0]
+
+im3_name = random_dict[2][1]
+im3_title = random_dict[2][0]
+
+print("Image 1: ", im1_title, im1_name, "\nImage 2: ", im2_title, im2_name, "\nImage 3: ", im3_title, im3_name)
