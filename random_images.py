cb7ff40775228ecce5caf39d7ca27245f91ad8fc
kyle-parker-1500 <kylerparker1500@gmail.com>
2025-10-31 08:11:16 -0700

    finished random 3 images algorithm

--- random_images.py
+++ random_images.py
@@ -7,10 +7,21 @@ from image_info import image_info as info
 # <img src="/static/images/{{ ... }}.jpg" class="w-50" /> -> keep in mind that this is how the html tag should look to get an img
 # Note the .jpg on the outside of the path -> so we're only passing the image id
 
+# returns dict of 3 selected random images & their titles
 def random_three():
-    # read dict:
     # need image id -> pass
     ids = [i['id'] for i in info]
     # need image title -> pass
-    name = [i['flickr_user']]
-    print(ids)
+    names = [i['title'] for i in info]
+    # shuffles names every time page is loaded
+    random.shuffle(names)
+    
+    list_images = {i['title'] : i['id'] for i in info}
+
+    # choose random 3 images
+    random_list = {}
+    for i in range(3):
+        random_list[names[i]] = list_images[names[i]]
+
+    return random_list
+
